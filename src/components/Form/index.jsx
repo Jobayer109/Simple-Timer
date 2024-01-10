8f8369c67483c7051069bfa5185480611808bb14
user.email <jobayer.ahmed109@gmail.com>
2024-01-10 18:44:22 +0600

    Form data handled and printed successfully

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,16 +1,35 @@
 import { Component } from "react";
 
 class MyForm extends Component {
+  state = {
+    name: "",
+    country: "",
+    bio: "",
+    birthDay: "",
+  };
+
+  onChangeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { name, country, bio, birthDay } = this.state;
     return (
       <div>
         <input
           className="form-control my-2"
           type="text"
           name="name"
+          value={name}
           placeholder="Your name hare"
+          onChange={this.onChangeHandler}
         />
-        <select name="country" className="form-control my-2">
+        <select
+          name="country"
+          value={country}
+          className="form-control my-2"
+          onChange={this.onChangeHandler}
+        >
           <option>Select a country</option>
           <option value="Bangladesh">Bangladesh</option>
           <option value="India">India</option>
@@ -21,9 +40,21 @@ class MyForm extends Component {
         <textarea
           className="form-control my-2"
           name="bio"
+          value={bio}
           placeholder="Write about yourself"
+          onChange={this.onChangeHandler}
         ></textarea>
-        <input className="form-control my-2" type="date" name="birthDay" />
+        <input
+          className="form-control my-2"
+          type="date"
+          name="birthDay"
+          value={birthDay}
+          onChange={this.onChangeHandler}
+        />
+
+        <div>
+          <button onClick={() => console.log(this.state)}>Show Data</button>
+        </div>
       </div>
     );
   }
